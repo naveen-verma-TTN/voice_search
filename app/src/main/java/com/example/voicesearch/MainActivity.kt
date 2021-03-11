5632f7544a2502db24e70a822bc5c5f84b103698
naveen.verma <naveen.verma@tothenew.com>
2021-03-11 17:44:02 +0530

    [Voice Search]: Added Permission request

--- app/src/main/java/com/example/voicesearch/MainActivity.kt
+++ app/src/main/java/com/example/voicesearch/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.voicesearch
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        private const val MY_PERMISSIONS_RECORD_AUDIO = 1
+    }
+
     var sheetBehavior: BottomSheetBehavior<*>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +33,25 @@ class MainActivity : AppCompatActivity() {
         sheetBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
-                    BottomSheetBehavior.STATE_HIDDEN -> { }
-                    BottomSheetBehavior.STATE_EXPANDED -> { }
-                    BottomSheetBehavior.STATE_COLLAPSED -> { }
-                    BottomSheetBehavior.STATE_DRAGGING -> { }
-                    BottomSheetBehavior.STATE_SETTLING -> { }
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> { }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
                 }
             }
+
             override fun onSlide(bottomSheet: View, slideOffset: Float) {}
         })
 
-        toggleBottomSheet()
+        requestAudioPermissions()
     }
 
     private fun toggleBottomSheet() {
@@ -47,9 +64,52 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        sheetBehavior?.removeBottomSheetCallback(object: BottomSheetCallback(){
+        sheetBehavior?.removeBottomSheetCallback(object : BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {}
             override fun onSlide(bottomSheet: View, slideOffset: Float) {}
         })
+    }
+
+    private fun requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.RECORD_AUDIO)) {
+                Utility.showToast(this, "Please grant permissions to record audio")
+
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO),
+                        MY_PERMISSIONS_RECORD_AUDIO)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO),
+                        MY_PERMISSIONS_RECORD_AUDIO)
+            }
+        } else if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            loadVoiceSearch()
+        }
+    }
+
+    private fun loadVoiceSearch() {
+        toggleBottomSheet()
+        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.voice_fragment, VoiceSearchFragment())
+        ft.commit()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_RECORD_AUDIO -> {
+                if (grantResults.isNotEmpty()
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadVoiceSearch()
+                } else {
+                    Utility.showToast(this, "Permissions Denied to record audio")
+                }
+                return
+            }
+        }
     }
 }
